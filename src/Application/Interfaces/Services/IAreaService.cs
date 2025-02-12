413bc13895f65e518c958d4de90720cf4acd089e
Andryel13 <andryel.passos@stellantis.com>
2025-02-12 11:17:04 -0300

    AMS-23: Criação de entidade e interaces

---
+++ src/Application/Interfaces/Services/IAreaService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stellantis.ProjectName.Domain.Entities;
+
+namespace Stellantis.ProjectName.Application.Interfaces.Services
+{
+    public interface IAreaService : IEntityServiceBase<Area>
+    {
+    }
+}
---
+++ src/Domain/Entities/Area.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellantis.ProjectName.Domain.Entities
+{
+   public  class Area : EntityBase
+    {
+        public string Name { get; set; }
+    }
+}
