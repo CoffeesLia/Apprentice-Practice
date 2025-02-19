e379186e74d06f09edacc04c4543158d16bd280e
SE68087 <patricia.neves@stellantis.com>
2025-02-19 09:50:26 -0300

    Implementação da listagem de area

---
+++ src/Application/Models/Filters/AreaFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellantis.ProjectName.Application.Models.Filters
+{
+    public class AreaFilter : Filter
+    {
+        public string? Name { get; set; }
+    }
+}
