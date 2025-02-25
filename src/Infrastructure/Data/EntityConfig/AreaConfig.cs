8d049fba231f336a5b093bad78003aaa63185b03
Andryel13 <andryel.passos@stellantis.com>
2025-02-25 11:37:23 -0300

    AMS-20-Implementação AreaConfig

--- src/Infrastructure/Data/EntityConfig/AreaConfig.cs
+++ src/Infrastructure/Data/EntityConfig/AreaConfig.cs
@@ -9,14 +9,15 @@ using Stellantis.ProjectName.Domain.Entities;
 
 namespace Stellantis.ProjectName.Infrastructure.Data.EntityConfig
 {
-    public class AreaConfig : IEntityTypeConfiguration<Area>
-    {
-        public void Configure(EntityTypeBuilder<Area> builder)
+
+        public class AreaConfig : IEntityTypeConfiguration<Area>
         {
-            ArgumentNullException.ThrowIfNull(builder);
-            builder.ToTable("Area");
-            builder.HasKey(p => p.Id);
-            builder.Property(p => p.Name).IsRequired().HasMaxLength(255);
+            public void Configure(EntityTypeBuilder<Area> builder)
+            {
+                ArgumentNullException.ThrowIfNull(builder);
+                builder.ToTable("Area");
+                builder.HasKey(p => p.Id);
+                builder.Property(p => p.Name).IsRequired().HasMaxLength(255);
+            }
         }
     }
-}
