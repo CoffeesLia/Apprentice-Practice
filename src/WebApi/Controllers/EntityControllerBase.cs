669d862badf3659ddef1ea756edca4dbc0472274
Jardelzin <jardelrodrigues5555@gmail.com>
2025-02-25 13:57:06 -0300

    Linha duplicada resolvido

--- src/WebApi/Controllers/EntityControllerBase.cs
+++ src/WebApi/Controllers/EntityControllerBase.cs
@@ -39,7 +39,7 @@ namespace Stellantis.ProjectName.WebApi.Controllers
             return result.Status switch
             {
 
-                OperationStatus.Success => CreatedAtAction(HttpMethods.Get, new { id = item!.Id }, Mapper.Map<TEntityVm>(item)),
+              
                 OperationStatus.Success => CreatedAtAction(HttpMethod.Get.Method, new { id = item!.Id} , Mapper.Map<TEntityVm>(item)),
                 OperationStatus.Conflict => Conflict(result),
                 OperationStatus.InvalidData => UnprocessableEntity(result),
