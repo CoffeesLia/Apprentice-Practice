d860e6781b670c4896e433a7bb939f9465a4b566
Jardelzin <jardelrodrigues5555@gmail.com>
2025-02-26 14:13:13 -0300

    Criar Squad

---
+++ src/Application.Tests/Services/SquadServiceTestes.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using Stellantis.ProjectName.Application.Interfaces.Services;
+using Stellantis.ProjectName.Application.Services;
+using Stellantis.ProjectName.Domain.Entities;
+using System;
+using Xunit;
+namespace Stellantis.ProjectName.Tests.Services
+{
+    public class SquadServiceTests
+    {
+        [Fact]
+        public void CreateSquad_ShouldThrowException_WhenNameIsEmpty()
+        {
+            var squadRepositoryMock = new Mock<ISquadRepository>();
+            var squadService = new SquadService(squadRepositoryMock.Object);
+
+            Assert.Throws<ArgumentException>(() => squadService.CreateSquad("", "Description"));
+        }
+
+        [Fact]
+        public void CreateSquad_ShouldThrowException_WhenDescriptionIsEmpty()
+        {
+            var squadRepositoryMock = new Mock<ISquadRepository>();
+            var squadService = new SquadService(squadRepositoryMock.Object);
+
+            Assert.Throws<ArgumentException>(() => squadService.CreateSquad("SquadName", ""));
+        }
+
+        [Fact]
+        public void CreateSquad_ShouldThrowException_WhenSquadNameAlreadyExists()
+        {
+            var squadRepositoryMock = new Mock<ISquadRepository>();
+            squadRepositoryMock.Setup(repo => repo.GetByName(It.IsAny<string>())).Returns(new EntitieSquad());
+
+            var squadService = new SquadService(squadRepositoryMock.Object);
+
+            Assert.Throws<InvalidOperationException>(() => squadService.CreateSquad("ExistingSquad", "Description"));
+        }
+
+        [Fact]
+        public void CreateSquad_ShouldAddSquad_WhenValid()
+        {
+            var squadRepositoryMock = new Mock<ISquadRepository>();
+            squadRepositoryMock.Setup(repo => repo.GetByName(It.IsAny<string>())).Returns((EntitieSquad)null);
+
+            var squadService = new SquadService(squadRepositoryMock.Object);
+
+            squadService.CreateSquad("NewSquad", "Description");
+
+            squadRepositoryMock.Verify(repo => repo.Add(It.IsAny<EntitieSquad>()), Times.Once);
+        }
+    }
+}
---
+++ src/Application/Interfaces/Services/ISquadRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stellantis.ProjectName.Domain.Entities;
+
+namespace Stellantis.ProjectName.Application.Interfaces.Services
+{
+    public interface ISquadRepository
+    {
+        void Add(EntitieSquad squad);
+        EntitieSquad GetByName(String name);
+        IEnumerable<EntitieSquad> GetAll();
+    }
+}
