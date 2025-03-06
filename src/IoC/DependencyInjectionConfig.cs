4479d92533781bf512d67d0ae750306c1f69cc6e
Welton Fernandes Duarte <welton.duarte@external.stellantis.com>
2025-03-06 10:55:11 -0300

    Correção de acesso a classes e injeção de dependência.

--- src/IoC/DependencyInjectionConfig.cs
+++ src/IoC/DependencyInjectionConfig.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Stellantis.ProjectName.Application.Interfaces;
 using Stellantis.ProjectName.Application.Interfaces.Repositories;
 using Stellantis.ProjectName.Application.Interfaces.Services;
 using Stellantis.ProjectName.Application.Services;
+using Stellantis.ProjectName.Application.Validators;
+using Stellantis.ProjectName.Domain.Entities;
+using Stellantis.ProjectName.Infrastructure.Data;
+using Stellantis.ProjectName.Infrastructure.Data.Repositories;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Stellantis.ProjectName.IoC
@@ -19,21 +25,18 @@ namespace Stellantis.ProjectName.IoC
         [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "It's code temporary.")]
         private static void Validators(IServiceCollection services)
         {
+            services.AddScoped<IValidator<Area>, AreaValidator>();
         }
 
-        [SuppressMessage("Critical Code Smell", "S1186:Methods should not be empty", Justification = "It's code temporary.")]
-        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "It's code temporary.")]
         private static void Services(IServiceCollection services)
         {
             services.AddScoped<IAreaService, AreaService>();
         }
 
-        [SuppressMessage("Critical Code Smell", "S1186:Methods should not be empty", Justification = "It's code temporary.")]
-        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "It's code temporary.")]
         private static void Repositories(IServiceCollection services)
         {
-           
-
+            services.AddScoped<IAreaRepository, AreaRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
 }
--- src/WebApi/Mapper/AutoMapperProfile.cs
+++ src/WebApi/Mapper/AutoMapperProfile.cs
@@ -14,9 +14,10 @@ namespace Stellantis.ProjectName.WebApi.Mapper
             CreateMap(typeof(PagedResult<>), typeof(PagedResultVm<>));
             CreateMap<AreaDto, Area>()
                 .ForMember(x => x.Id, x => x.Ignore());
-            CreateMap<Area, AreaVm>();
+            CreateMap<Area, AreaVm>()
+                .ForMember(x => x.Applications, x => x.Ignore());
             CreateMap<AreaFilterDto, AreaFilter>();
-           
+
         }
     }
 }
