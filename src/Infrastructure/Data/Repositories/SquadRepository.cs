e8171a4c5d64fe4eb4ec9d310c5dd0fac7265e25
Jardelzin <jardelrodrigues5555@gmail.com>
2025-03-19 13:30:23 -0300

    correçao de teste

---
+++ src/Infrastructure/Data/Repositories/SquadRepository.cs
@@ -0,0 +1,47 @@
+﻿using Stellantis.ProjectName.Application.Interfaces.Repositories;
+using Stellantis.ProjectName.Domain.Entities;
+using Stellantis.ProjectName.Domain.Entity;
+using Stellantis.ProjectName.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stellantis.ProjectName.Infrastructure.Repositories
+{
+    public class SquadRepository : ISquadRepository
+    {
+        private readonly Context _context;
+
+        public SquadRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Add(EntitySquad squad)
+        {
+            _context.Squads.Add(squad);
+            _context.SaveChanges();
+        }
+
+        public EntitySquad GetByName(string name)
+        {
+            return _context.Squads.FirstOrDefault(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IEnumerable<EntitySquad> GetAll()
+        {
+            return _context.Squads.ToList();
+        }
+
+        public EntitySquad GetById(Guid id)
+        {
+            return _context.Squads.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Update(EntitySquad squad)
+        {
+            _context.Squads.Update(squad);
+            _context.SaveChanges();
+        }
+    }
+}
