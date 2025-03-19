05aa1dc8038cd972371772a5fa54e17cf0d95234
Andryel13 <andryel.passos@stellantis.com>
2025-03-19 09:09:13 -0300

    Bugfix- Apagando createasync de Irepository

--- src/Application/Interfaces/Repositories/IRepositoryEntityBase.cs
+++ src/Application/Interfaces/Repositories/IRepositoryEntityBase.cs
@@ -6,7 +6,6 @@ namespace Stellantis.ProjectName.Application.Interfaces.Repositories
 {
     public interface IRepositoryEntityBase<TEntity> : IRepositoryBase<TEntity> where TEntity : EntityBase
     {
-        Task<PagedResult<Integration>> CreateAsync(Integration integration);
         Task DeleteAsync(int id, bool saveChanges = true);
         Task<TEntity?> GetByIdAsync(int id);
         Task<PagedResult<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? filter = null, string? sort = null, string? sortDir = null, string? includeProperties = null, int page = 1, int pageSize = 10);
