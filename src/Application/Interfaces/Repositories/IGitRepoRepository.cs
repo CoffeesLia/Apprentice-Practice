44f08e3252bda635ccf144f8c12f8aa10fcca324
Cecilia.Melgaco <SE66163@stellantis.com>
2025-03-20 14:11:50 -0300

    update 2

--- src/Application/Interfaces/Repositories/IGitRepoRepository.cs
+++ src/Application/Interfaces/Repositories/IGitRepoRepository.cs
@@ -1,17 +1,16 @@
 ﻿using Stellantis.ProjectName.Application.Models.Filters;
 using Stellantis.ProjectName.Application.Models;
 using Stellantis.ProjectName.Domain.Entities;
-using Stellantis.ProjectName.Application.Interfaces.Services;
 using System.Linq.Expressions;
 
 namespace Stellantis.ProjectName.Application.Interfaces.Repositories
 {
     public interface IGitRepoRepository : IRepositoryEntityBase<GitRepo>
     {
-        Task<PagedResult<GitRepo>> GetListAsync(GitLabFilter filter);
+        Task<PagedResult<GitRepo>> GetListAsync(GitRepoFilter filter);
         IAsyncEnumerable<GitRepo> ListRepositories();
         Task<GitRepo?> GetRepositoryDetailsAsync(int id);
-        new Task<OperationResult> CreateAsync(GitRepo newRepo);
+        Task<OperationResult> CreateAsync(GitRepo newRepo);
         Task<bool> VerifyAplicationsExistsAsync(int id);
         Task<bool> AnyAsync(Expression<Func<GitRepo, bool>> expression); // Adicionado
     }
--- src/Application/Models/Filters/GitRepoFilter.cs
+++ src/Application/Models/Filters/GitRepoFilter.cs
@@ -2,9 +2,9 @@
 {
     public class GitLabFilter : Filter
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Url { get; set; }
         public int ApplicationId { get; set; }
     }
 }
