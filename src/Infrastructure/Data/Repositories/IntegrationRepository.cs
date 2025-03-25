763d13f9a29b8377845e4c90a2e78a6af5a5d854
Welton Fernandes Duarte <welton.duarte@external.stellantis.com>
2025-03-25 08:56:53 -0300

    Correção do merge errado.

--- src/Infrastructure/Data/Repositories/IntegrationRepository.cs
+++ src/Infrastructure/Data/Repositories/IntegrationRepository.cs
@@ -9,51 +9,48 @@ using Stellantis.ProjectName.Application.Resources;
 namespace Stellantis.ProjectName.Infrastructure.Data.Repositories
 {
     public class IntegrationRepository(Context context) : RepositoryBase<Integration, Context>(context), IIntegrationRepository
-    {     
-        public IntegrationRepository(Context context) : base(context) { }
-        
+    {
         public async Task DeleteAsync(int id, bool saveChanges = true)
+        {
+            var integration = await GetByIdAsync(id).ConfigureAwait(false);
+            if (integration != null)
             {
-                var integration = await GetByIdAsync(id).ConfigureAwait(false);
-                if (integration != null)
+                Context.Set<Integration>().Remove(integration);
+                if (saveChanges)
                 {
-                    Context.Set<Integration>().Remove(integration);
-                    if (saveChanges)
-                    {
-                        await Context.SaveChangesAsync().ConfigureAwait(false);
-                    }
+                    await Context.SaveChangesAsync().ConfigureAwait(false);
                 }
             }
-            public async Task<Integration?> GetByIdAsync(int id)
+        }
+        public async Task<Integration?> GetByIdAsync(int id)
+        {
+            return await Context.Set<Integration>().FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<PagedResult<Integration>> GetListAsync(IntegrationFilter filter)
+        {
+            IQueryable<Integration> query = Context.Set<Integration>();
+
+            if (!string.IsNullOrEmpty(filter.Name))
             {
-                return await Context.Set<Integration>().FindAsync(id).ConfigureAwait(false);
+                query = query.Where(a => a.Name.Contains(filter.Name));
             }
 
-            public async Task<PagedResult<Integration>> GetListAsync(IntegrationFilter filter)
+            return await GetPagedResultAsync(query, filter.Page, filter.PageSize).ConfigureAwait(false);
+        }
+
+        private static async Task<PagedResult<Integration>> GetPagedResultAsync(IQueryable<Integration> query, int page, int pageSize)
+        {
+            var total = await query.CountAsync().ConfigureAwait(false);
+            var result = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync().ConfigureAwait(false);
+
+            return new PagedResult<Integration>
             {
-                IQueryable<Integration> query = Context.Set<Integration>();
-
-                if (!string.IsNullOrEmpty(filter.Name))
-                {
-                    query = query.Where(a => a.Name.Contains(filter.Name));
-                }
-
-                return await GetPagedResultAsync(query, filter.Page, filter.PageSize).ConfigureAwait(false);
-            }
-
-            private static async Task<PagedResult<Integration>> GetPagedResultAsync(IQueryable<Integration> query, int page, int pageSize)
-            {
-                var total = await query.CountAsync().ConfigureAwait(false);
-                var result = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync().ConfigureAwait(false);
-
-                return new PagedResult<Integration>
-                {
-                    Total = total,
-                    Result = result,
-                    Page = page,
-                    PageSize = pageSize
-                };
-        }   }
+                Total = total,
+                Result = result,
+                Page = page,
+                PageSize = pageSize
+            };
+        }
     }
 }
-
