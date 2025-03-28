74f3ab519ab6c1667118c4261dd9b18e3c73b389
Cecilia.Melgaco <SE66163@stellantis.com>
2025-03-28 09:40:45 -0300

    GitRepoServiceTests - Update & Delete

--- src/Infrastructure/Data/EntityConfig/GitRepoConfig.cs
+++ src/Infrastructure/Data/EntityConfig/GitRepoConfig.cs
@@ -1,7 +1,6 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Stellantis.ProjectName.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace Stellantis.ProjectName.Infrastructure.Data.EntityConfig
 {
@@ -11,10 +10,9 @@ namespace Stellantis.ProjectName.Infrastructure.Data.EntityConfig
         public void Configure(EntityTypeBuilder<GitRepo> builder)
         {
             ArgumentNullException.ThrowIfNull(builder);
-            builder.ToTable("GitLabRepository");
+            builder.ToTable("GitRepoRepository");
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Name).IsRequired().HasMaxLength(255);
         }
     }
 }
-
