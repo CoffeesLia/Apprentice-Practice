4bd151ec53194fcd840205f8d71073f49051380c
Andryel13 <andryel.passos@stellantis.com>
2025-04-01 10:32:58 -0300

    criação de testes para web-api

--- src/WebApi.Tests/Controllers/IntegrationControllerTests.cs
+++ src/WebApi.Tests/Controllers/IntegrationControllerTests.cs
@@ -10,6 +10,7 @@ using Stellantis.ProjectName.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Stellantis.ProjectName.WebApi.Dto.Filters;
 using Stellantis.ProjectName.Application.Models.Filters;
+using AutoFixture;
 
 
 namespace WebApi.Tests.Controllers
@@ -22,18 +23,13 @@ namespace WebApi.Tests.Controllers
         private readonly IntegrationController _controller;
 
         public IntegrationControllerTests()
-
-        private readonly IntegrationControllerBase _controller;
-        private readonly Fixture _fixture;
-    
-    public IntegrationControllerTests()
         {
             _serviceMock = new Mock<IIntegrationService>();
             _mapperMock = new Mock<IMapper>();
             _localizerFactoryMock = new Mock<IStringLocalizerFactory>();
-
             _controller = new IntegrationController(_serviceMock.Object, _mapperMock.Object, _localizerFactoryMock.Object);
         }
+
         [Fact]
         public async Task CreateAsyncShouldReturnCreatedAtActionResultWhenIntegrationIsValid()
         {
@@ -59,6 +55,7 @@ namespace WebApi.Tests.Controllers
             // Assert
             Assert.IsType<NotFoundResult>(result);
         }
+
         [Fact]
         public async Task DeleteAsyncShouldReturnNoContentWhenCalled()
         {
@@ -69,6 +66,7 @@ namespace WebApi.Tests.Controllers
             // Assert
             Assert.IsType<NoContentResult>(result);
         }
+
         [Fact]
         public async Task CreateAsyncShouldReturnConflictWhenOperationIsConflict()
         {
@@ -89,7 +87,6 @@ namespace WebApi.Tests.Controllers
         }
 
         [Fact]
-
         public async Task UpdateAsyncShoulReturnConflictWhenOperationIsConflict()
         {
             // Arrange
@@ -104,6 +101,7 @@ namespace WebApi.Tests.Controllers
             var conflictResult = Assert.IsType<ConflictObjectResult>(result);
             Assert.Equal(operationResult, conflictResult.Value);
         }
+
         [Fact]
         public async Task UpdateAsyncShouldReturnNotFoundWhenIntegrationDoesNotExist()
         {
@@ -128,6 +126,7 @@ namespace WebApi.Tests.Controllers
             // Assert
             Assert.IsType<NotFoundResult>(result.Result);
         }
+
         [Fact]
         public async Task GetAsyncShouldReturnIntegrationVmWhenIntegrationExists()
         {
@@ -143,6 +142,7 @@ namespace WebApi.Tests.Controllers
             var model = Assert.IsType<IntegrationVm>(okResult.Value);
             Assert.Equal(integrationVm, model);
         }
+
         [Fact]
         public async Task GetListAsyncShouldReturnPagedResultVmWhenCalled()
         {
@@ -164,6 +164,7 @@ namespace WebApi.Tests.Controllers
             var model = Assert.IsType<PagedResultVm<IntegrationVm>>(okResult.Value);
             Assert.Equal(pagedResultVm, model);
         }
+
         [Fact]
         public async Task GetListAsyncShouldReturnPagedResultVmWhenCalledWithNullFilter()
         {
@@ -180,32 +181,6 @@ namespace WebApi.Tests.Controllers
             var okResult = Assert.IsType<OkObjectResult>(result);
             var model = Assert.IsType<PagedResultVm<IntegrationVm>>(okResult.Value);
             Assert.Equal(pagedResultVm, model);
-            _fixture = new Fixture();
-            _controller = new IntegrationControllerBase(_serviceMock.Object, _mapperMock.Object, _localizerFactoryMock.Object);
-        }
-        
-        [Fact]
-
-        public async Task CreateAsyncShouldReturnCreatedAtActionWhenCreationIsSuccessful()
-        {
-            // Arrange
-            var integrationDto = _fixture.Create<IntegrationDto>();
-            var integration = _fixture.Build<Integration>().With(i => i.Name, integrationDto.Name).Create();
-            var integrationVm = _fixture.Build<IntegrationVm>().With(i => i.Name, integrationDto.Name).With(i => i.Id, integration.Id).Create();
-
-            _mapperMock.Setup(m => m.Map<Integration>(integrationDto)).Returns(integration);
-            _serviceMock.Setup(s => s.CreateAsync(integration)).ReturnsAsync(OperationResult.Complete("Success"));
-            _mapperMock.Setup(m => m.Map<IntegrationVm>(integration)).Returns(integrationVm);
-
-            // Act
-            var result = await _controller.CreateAsync(integrationDto);
-
-            // Assert
-            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
-            Assert.Equal(nameof(_controller.GetAsync), createdAtActionResult.ActionName);
-            Assert.Equal(integration.Id, createdAtActionResult.RouteValues["id"]);
-            Assert.Equal(integrationVm, createdAtActionResult.Value);
-
         }
     }
 }
