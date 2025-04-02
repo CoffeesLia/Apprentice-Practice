06f9fd2e65d21620e364a863438103fc08cdefe9
Matheus Fernandes <SE68084@stellantis.com>
2025-04-02 09:37:58 -0300

    Revert "Rebuild develop"
    This reverts commit 4a51fde48811b28ef184dbc3ff090edf57978f07.

---
+++ src/Domain/Entities/EntityGitLabRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stellantis.ProjectName.Domain.Entities
+{
+    public class EntityGitLabRepository : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public int ApplicationId { get; set; }
+    }
+
+}
