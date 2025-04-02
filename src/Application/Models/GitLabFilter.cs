56775d8aa3c1c3d207cb72851c127cf39be79c6b
Matheus Fernandes <SE68084@stellantis.com>
2025-04-02 09:46:30 -0300

    Rebuild develop

--- src/Application/Models/GitLabFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Stellantis.ProjectName.Filters
-{
-    public class GitLabFilter
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
-    }
-}
