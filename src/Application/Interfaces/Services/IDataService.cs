78c08fc30be5b27a6b454f4dc378916de6ea0cd4
Matheus Fernandes <SE68084@stellantis.com>
2025-04-04 09:33:46 -0300

    IDataService update

--- src/Application/Interfaces/Services/IDataService.cs
+++ src/Application/Interfaces/Services/IDataService.cs
@@ -1,9 +1,10 @@
-﻿using Stellantis.ProjectName.Application.Interfaces.Repositories;
+﻿using Stellantis.ProjectName.Application.Models.Filters;
 using Stellantis.ProjectName.Domain.Entities;
 
 namespace Stellantis.ProjectName.Application.Interfaces.Services
 {
-    public interface IDataService : IDataServiceRepository
+    public interface IDataService : IEntityServiceBase<DataService>
     {
+        Task<PagedResult<DataService>> GetListAsync(DataServiceFilter serviceFilter);
     }
 }
