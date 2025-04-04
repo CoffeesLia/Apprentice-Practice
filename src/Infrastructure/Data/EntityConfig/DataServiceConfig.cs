ce5892a7689891b1ed350495649fdec1b0d167ed
Matheus Fernandes <SE68084@stellantis.com>
2025-04-04 09:28:07 -0300

    DataServiceConfig update

--- src/Infrastructure/Data/EntityConfig/DataServiceConfig.cs
+++ src/Infrastructure/Data/EntityConfig/DataServiceConfig.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Stellantis.ProjectName.Application.Interfaces.Repositories;
-using Stellantis.ProjectName.Application.Interfaces.Services;
-using Stellantis.ProjectName.Application.Services;
 using Stellantis.ProjectName.Domain.Entities;
-using Stellantis.ProjectName.Infrastructure.Data.Repositories;
 
 namespace Stellantis.ProjectName.Infrastructure.Data.EntityConfig
 {
-    public class DataServiceConfig : IEntityTypeConfiguration<EDataService>
+    public class DataServiceConfig : IEntityTypeConfiguration<DataService>
     {
-        public void Configure(EntityTypeBuilder<EDataService> builder)
+        public void Configure(EntityTypeBuilder<DataService> builder)
         {
             ArgumentNullException.ThrowIfNull(builder);
 
-            builder.ToTable("DataService");
+            builder.ToTable("Service");
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Name)
                 .IsRequired()
-                .HasMaxLength(255);
+                .HasMaxLength(50);
         }
     }
 }
