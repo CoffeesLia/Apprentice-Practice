f33bebcde752c552e32d30084d0d8aa2e8861f71
Matheus Fernandes <SE68084@stellantis.com>
2025-04-04 09:30:20 -0300

    DataService update

--- src/Domain/Entities/DataService.cs
+++ src/Domain/Entities/DataService.cs
@@ -4,7 +4,6 @@
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public int ApplicationId { get; set; }
+        public int ServiceId { get; set; }
     }
-
 }
