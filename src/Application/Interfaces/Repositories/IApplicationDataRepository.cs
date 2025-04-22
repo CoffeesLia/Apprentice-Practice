f4881c26617471598f3dc13bab68d2321cf60b84
SE67097 <vitoria.leoncio@stellantis.com>
2025-04-22 14:09:52 -0300

    correçao

--- src/Application/Interfaces/Repositories/IApplicationDataRepository.cs
+++ src/Application/Interfaces/Repositories/IApplicationDataRepository.cs
@@ -10,7 +10,6 @@ namespace Stellantis.ProjectName.Application.Interfaces.Repositories
 {
     public interface IApplicationDataRepository : IRepositoryEntityBase<ApplicationData>
     {
-        //teste
         Task<PagedResult<ApplicationData>> GetListAsync(ApplicationFilter applicationFilter);
         Task<bool> IsApplicationNameUniqueAsync(string name, int? id = null);
         Task<ApplicationData?> GetFullByIdAsync(int id);
