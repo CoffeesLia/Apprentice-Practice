0f46a67392c41eaffc5e3364cc238e677d53e32c
Jardelzin <jardelrodrigues5555@gmail.com>
2025-04-23 09:29:15 -0300

    correção testes serviços e correção do validator

--- src/Application/Validators/SquadValidator.cs
+++ src/Application/Validators/SquadValidator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentValidation;
 using Microsoft.Extensions.Localization;
-using Stellantis.ProjectName.Application.Interfaces.Services;
 using Stellantis.ProjectName.Application.Resources;
 using Stellantis.ProjectName.Domain.Entities;
 
@@ -15,16 +10,37 @@ namespace Stellantis.ProjectName.Application.Validators
     {
         internal const int MinimumLength = 3;
         internal const int MaximumLength = 255;
+
         public SquadValidator(IStringLocalizerFactory localizerFactory)
         {
             ArgumentNullException.ThrowIfNull(localizerFactory);
             var localizer = localizerFactory.Create(typeof(SquadResources));
 
+           
             RuleFor(x => x.Name)
-               .MinimumLength(MinimumLength)
-               .WithMessage(localizer[nameof(SquadResources.NameValidateLength), MinimumLength, MaximumLength])
-               .MaximumLength(MaximumLength)
-               .WithMessage(localizer[nameof(SquadResources.NameValidateLength), MinimumLength, MaximumLength]);
+                .NotEmpty()
+                .WithMessage(localizer[nameof(SquadResources.SquadNameRequired)]) 
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.Name)
+                        .MinimumLength(MinimumLength)
+                        .WithMessage(localizer[nameof(SquadResources.NameValidateLength), MinimumLength, MaximumLength])
+                        .MaximumLength(MaximumLength)
+                        .WithMessage(localizer[nameof(SquadResources.NameValidateLength), MinimumLength, MaximumLength]);
+                });
+
+            
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage(localizer[nameof(SquadResources.SquadDescriptionRequired)]) 
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.Description)
+                        .MinimumLength(MinimumLength)
+                        .WithMessage(localizer[nameof(SquadResources.DescriptionValidateLength), MinimumLength, MaximumLength])
+                        .MaximumLength(MaximumLength)
+                        .WithMessage(localizer[nameof(SquadResources.DescriptionValidateLength), MinimumLength, MaximumLength]);
+                });
         }
     }
 }
