f2e1cf5fbcedc9b4dfaacb06b24601973ee34285
Matheus Fernandes <SE68084@stellantis.com>
2025-04-28 13:33:51 -0300

    ServiceDataDto update

--- src/WebApi/Dto/ServiceDataDto.cs
+++ src/WebApi/Dto/ServiceDataDto.cs
@@ -4,7 +4,6 @@
     {
         public required string Name { get; set; }
         public string? Description { get; set; }
-        public int Id { get; set; }
         public int ApplicationId { get; set; }
     }
 }
