160bcc2ff68181317ab63b5706ef509631e28bec
SE68087 <patricia.neves@stellantis.com>
2025-04-29 13:46:32 -0300

    GetList Repositório

--- src/Infrastructure/Data/Repositories/ResponsibleRepository.cs
+++ src/Infrastructure/Data/Repositories/ResponsibleRepository.cs
@@ -24,9 +24,14 @@ namespace Stellantis.ProjectName.Infrastructure.Data.Repositories
             if (filter.AreaId != 0)
                 filters = filters.And(x => x.AreaId == filter.AreaId);
 
-            return await GetListAsync(filter: filters, page: filter.Page, sort: filter.Sort, sortDir: filter.SortDir).ConfigureAwait(false);
+            return await GetListAsync(
+                filter: filters,
+                page: filter.Page,
+                sort: filter.Sort,
+                sortDir: filter.SortDir,
+                includeProperties: nameof(Responsible.Area)
+                ).ConfigureAwait(false);
         }
-
         public async Task<bool> VerifyEmailAlreadyExistsAsync(string email)
         {
             return await Context.Set<Responsible>().AnyAsync(r => r.Email == email).ConfigureAwait(false);
