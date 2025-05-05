46fda07bb57893b1aec947e3fbf7d3a7b85ef9d3
SE68087 <patricia.neves@stellantis.com>
2025-05-05 09:14:25 -0300

    correção da paginação

--- src/Infrastructure/Data/Repositories/ResponsibleRepository.cs
+++ src/Infrastructure/Data/Repositories/ResponsibleRepository.cs
@@ -11,20 +11,21 @@ namespace Stellantis.ProjectName.Infrastructure.Data.Repositories
         public ResponsibleRepository(Context context) : base(context)
         {
         }
-        public async Task<PagedResult<Responsible>> GetListAsync(ResponsibleFilter filter)
+        public async Task<PagedResult<Responsible>> GetListAsync(ResponsibleFilter responsibleFilter)
         {
-            ArgumentNullException.ThrowIfNull(filter);
+            ArgumentNullException.ThrowIfNull(responsibleFilter);
 
             var filters = PredicateBuilder.New<Responsible>(true);
+            responsibleFilter.Page = responsibleFilter.Page <= 0 ? 1 : responsibleFilter.Page;
 
-            if (!string.IsNullOrWhiteSpace(filter.Email))
-                filters = filters.And(x => x.Email.Contains(filter.Email));
-            if (!string.IsNullOrWhiteSpace(filter.Name))
-                filters = filters.And(x => x.Name.Contains(filter.Name));
-            if (filter.AreaId != 0)
-                filters = filters.And(x => x.AreaId == filter.AreaId);
+            if (!string.IsNullOrWhiteSpace(responsibleFilter.Email))
+                filters = filters.And(x => x.Email.Contains(responsibleFilter.Email));
+            if (!string.IsNullOrWhiteSpace(responsibleFilter.Name))
+                filters = filters.And(x => x.Name.Contains(responsibleFilter.Name));
+            if (responsibleFilter.AreaId != 0)
+                filters = filters.And(x => x.AreaId == responsibleFilter.AreaId);
 
-            return await GetListAsync(filter: filters, page: filter.Page, sort: filter.Sort, sortDir: filter.SortDir).ConfigureAwait(false);
+            return await GetListAsync(filter: filters, page: responsibleFilter.Page, sort: responsibleFilter.Sort, sortDir: responsibleFilter.SortDir).ConfigureAwait(false);
         }
 
         public async Task<bool> VerifyEmailAlreadyExistsAsync(string email)
