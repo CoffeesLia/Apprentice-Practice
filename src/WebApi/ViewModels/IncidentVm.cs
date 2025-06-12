06808494836726386381126cd5f738d92848d3e0
Matheus Fernandes <SE68084@stellantis.com>
2025-06-12 14:32:22 -0300

    update feedback

--- src/WebApi/ViewModels/IncidentVm.cs
+++ src/WebApi/ViewModels/IncidentVm.cs
@@ -2,14 +2,13 @@
 {
     public class IncidentVm : EntityVmBase
     {
-        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime? ClosedAt { get; set; }
         public string Status { get; set; } = string.Empty;
         public int ApplicationId { get; set; }
-        public ApplicationVm Application { get; set; }
+        public ApplicationVm? Application { get; set; }
         public IEnumerable<int> MemberIds { get; set; } = [];
     }
 }
