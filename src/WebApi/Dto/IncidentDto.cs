fc29792b58866c431d32d474071357c71b83d5f2
SE68087 <patricia.neves@stellantis.com>
2025-06-16 11:29:05 -0300

    Correção erro 500

--- src/WebApi/Dto/IncidentDto.cs
+++ src/WebApi/Dto/IncidentDto.cs
@@ -1,12 +1,20 @@
 ﻿namespace Stellantis.ProjectName.WebApi.Dto
 {
+    public enum IncidentStatus
+    {
+        Open,
+        InProgress,
+        Cancelled,
+        Closed,
+        Reopened
+    }
     public class IncidentDto
     {
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int ApplicationId { get; set; }
         public IEnumerable<int> MemberIds { get; set; } = [];
-        public string Status { get; set; } = string.Empty;
+        public IncidentStatus Status { get; set; }
 
     }
 }
