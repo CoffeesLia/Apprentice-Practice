599e71fe517beed880934ef085a331a7530688bb
Andryel Passos <andryel.passos@stellantis.com>
2025-07-14 09:54:43 -0300

    implementação de enum para tipos da mensagem

--- src/Domain/Entities/SignalRNotification.cs
+++ src/Domain/Entities/SignalRNotification.cs
@@ -1,10 +1,12 @@
+using Stellantis.ProjectName.Domain.Enums;
+
 namespace Stellantis.ProjectName.Domain.Entities
 {
     public class SignalRNotification
     {
         public string? Title { get; set; }
         public string? Message { get; set; }
-        public string Type { get; set; } = "info";
+        public string Type { get; set; } = NotificationType.Info.ToString();
         public DateTime Timestamp { get; set; } = DateTime.Now;
         public Dictionary<string, object> Data { get; } = new(); 
     }
