d79f9590e4e6504e202ba6bbbcbb0546e9dcde74
Matheus Fernandes <SE68084@stellantis.com>
2025-07-24 11:26:37 -0300

    Sistema de notificações para Incidentes criado.

--- src/Application/Interfaces/Services/INotificationService.cs
+++ src/Application/Interfaces/Services/INotificationService.cs
@@ -1,8 +1,11 @@
-﻿namespace Stellantis.ProjectName.Application.Interfaces.Services
+﻿using Stellantis.ProjectName.Domain.Entities;
+
+namespace Stellantis.ProjectName.Application.Interfaces.Services
 {
     public interface INotificationService
     {
         Task NotifyIncidentCreatedAsync(int incidentId);
         Task NotifyIncidentStatusChangeAsync(int incidentId);
+        Task NotifyMembersAsync(IEnumerable<Member> members, string message); 
     }
 }
