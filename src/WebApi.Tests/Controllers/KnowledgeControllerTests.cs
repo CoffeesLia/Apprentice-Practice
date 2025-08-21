d0a6ca5437db99fbf97ccb48aa13d76caffabe33
ANA RAQUEL MORAIS DE SOUZA <SE68960@stellantis.com>
2025-08-21 14:38:07 -0300

    update branch

--- src/WebApi.Tests/Controllers/KnowledgeControllerTests.cs
+++ src/WebApi.Tests/Controllers/KnowledgeControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Stellantis.ProjectName.Application.Interfaces.Services;
 using Stellantis.ProjectName.Application.Models;
+using Stellantis.ProjectName.Application.Models.Filters;
 using Stellantis.ProjectName.Domain.Entities;
 using Stellantis.ProjectName.WebApi.Controllers;
 using Stellantis.ProjectName.WebApi.Dto;
@@ -27,7 +28,7 @@ namespace WebApi.Tests.Controllers
         }
 
         [Fact]
-        public async Task UpdateAsyncShouldReturnOkResultWhenUserIsNotSquadLeader()
+        public async Task UpdateAsyncShouldReturnOkResultWhenUpdateIsSuccessful()
         {
             // Arrange
             int knowledgeId = 1;
@@ -35,10 +36,10 @@ namespace WebApi.Tests.Controllers
             {
                 MemberId = 2,
                 ApplicationId = 3,
-                SquadId = 4
+                SquadId = 4,
+                Status = KnowledgeStatus.Atual
             };
 
-            // simula que o usuário não é líder do squad, mas o serviço retorna sucesso
             _serviceMock.Setup(s => s.UpdateAsync(It.IsAny<Knowledge>())).ReturnsAsync(OperationResult.Complete());
 
             // Act
@@ -46,61 +47,6 @@ namespace WebApi.Tests.Controllers
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
-        }
-
-        [Fact]
-        public async Task DeleteAsyncShouldReturnConflictWhenUserIsNotSquadLeader()
-        {
-            // Arrange
-            int knowledgeId = 1;
-
-            // simula que o usuário não é líder do squad
-            _serviceMock.Setup(s => s.DeleteAsync(It.IsAny<int>()))
-                .ReturnsAsync(OperationResult.Conflict("Usuário não é líder do squad"));
-
-            // Act
-            IActionResult result = await _controller.DeleteAsync(knowledgeId);
-
-            // Assert
-            Assert.IsType<ConflictObjectResult>(result);
-        }
-
-        [Fact]
-        public async Task UpdateAsyncShouldReturnOkResultWhenUserIsSquadLeader()
-        {
-            // Arrange
-            int knowledgeId = 1;
-            KnowledgeDto knowledgeDto = new()
-            {
-                MemberId = 2,
-                ApplicationId = 3,
-                SquadId = 4
-            };
-
-            // simula que o usuário é líder do squad
-            _serviceMock.Setup(s => s.UpdateAsync(It.IsAny<Knowledge>())).ReturnsAsync(OperationResult.Complete());
-
-            // Act
-            IActionResult result = await _controller.UpdateAsync(knowledgeId, knowledgeDto);
-
-            // Assert
-            Assert.IsType<OkObjectResult>(result);
-        }
-
-        [Fact]
-        public async Task DeleteAsyncShouldReturnNoContentWhenUserIsSquadLeader()
-        {
-            // Arrange
-            int knowledgeId = 1;
-
-            // simula que o usuário é líder do squad
-            _serviceMock.Setup(s => s.DeleteAsync(knowledgeId)).ReturnsAsync(OperationResult.Complete());
-
-            // Act
-            IActionResult result = await _controller.DeleteAsync(knowledgeId);
-
-            // Assert
-            Assert.IsType<NoContentResult>(result);
         }
 
         [Fact]
@@ -111,18 +57,33 @@ namespace WebApi.Tests.Controllers
             {
                 MemberId = 1,
                 ApplicationId = 2,
-                SquadId = 99 // Squad diferente do atual
+                SquadId = 99,
+                Status = KnowledgeStatus.Passado
             };
 
             _serviceMock
                 .Setup(s => s.UpdateAsync(It.IsAny<Knowledge>()))
-                .ReturnsAsync(OperationResult.Conflict("It's not allowed to remove past associations."));
+                .ReturnsAsync(OperationResult.Conflict("It's not allowed to update past associations."));
 
             // Act
             IActionResult result = await _controller.UpdateAsync(1, knowledgeDto);
 
             // Assert
             Assert.IsType<ConflictObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteAsyncShouldReturnNoContentWhenDeleteIsSuccessful()
+        {
+            // Arrange
+            int knowledgeId = 1;
+            _serviceMock.Setup(s => s.DeleteAsync(knowledgeId)).ReturnsAsync(OperationResult.Complete());
+
+            // Act
+            IActionResult result = await _controller.DeleteAsync(knowledgeId);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
         }
 
         [Fact]
@@ -139,6 +100,75 @@ namespace WebApi.Tests.Controllers
 
             // Assert
             Assert.IsType<ConflictObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateAsyncShouldReturnOkResultWhenCreateIsSuccessful()
+        {
+            // Arrange
+            var knowledgeDto = new KnowledgeDto
+            {
+                MemberId = 2,
+                ApplicationId = 3,
+                SquadId = 4,
+                Status = KnowledgeStatus.Atual
+            };
+
+            _serviceMock.Setup(s => s.CreateAsync(It.IsAny<Knowledge>())).ReturnsAsync(OperationResult.Complete());
+
+            // Act
+            IActionResult result = await _controller.CreateAsync(knowledgeDto);
+
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(result);
+        }
+
+        [Fact]
+        public async Task AssociateMultipleShouldReturnOkWhenAllAssociationsAreCreated()
+        {
+            // Arrange
+            var items = new List<KnowledgeDto>
+            {
+                new KnowledgeDto { MemberId = 1, ApplicationId = 2, SquadId = 3, Status = KnowledgeStatus.Atual },
+                new KnowledgeDto { MemberId = 1, ApplicationId = 5, SquadId = 3, Status = KnowledgeStatus.Passado }
+            };
+
+            _serviceMock.Setup(s => s.CreateAsync(It.IsAny<Knowledge>())).ReturnsAsync(OperationResult.Complete());
+
+            // Act
+            IActionResult result = await _controller.AssociateMultiple(items);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task GetAllByMemberShouldReturnOkResultWithList()
+        {
+            // Arrange
+            int memberId = 1;
+            var knowledges = new List<Knowledge>
+            {
+                new Knowledge { MemberId = 1, ApplicationId = 2, SquadId = 3, Status = KnowledgeStatus.Atual },
+                new Knowledge { MemberId = 1, ApplicationId = 5, SquadId = 3, Status = KnowledgeStatus.Passado }
+            };
+            var pagedResult = new PagedResult<Knowledge>
+            {
+                Result = knowledges,
+                Page = 1,
+                PageSize = 10,
+                Total = 2
+            };
+
+            _serviceMock.Setup(s => s.GetListAsync(It.IsAny<KnowledgeFilter>())).ReturnsAsync(pagedResult);
+
+            // Act
+            IActionResult result = await _controller.GetAllByMember(memberId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedList = Assert.IsAssignableFrom<List<KnowledgeVm>>(okResult.Value);
+            Assert.Equal(2, returnedList.Count);
         }
     }
 }
