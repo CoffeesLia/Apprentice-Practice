709689c900fce97a0ecb5b56727db44df09e841c
Cecilia.Melgaco <SE66163@stellantis.com>
2025-08-28 10:19:56 -0300

    Injeção de dependência

---
+++ src/WebApi/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stellantis.ProjectName.Application.Interfaces.Services;
+
+namespace Stellantis.ProjectName.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class DashboardController(IDashboardService dashboardService) : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService = dashboardService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardData()
+        {
+            var dashboard = await _dashboardService.GetDashboardDataAsync().ConfigureAwait(false);
+            return Ok(dashboard);
+        }
+    }
+}
